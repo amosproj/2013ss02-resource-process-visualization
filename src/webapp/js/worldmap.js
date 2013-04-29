d7b2249ddfab6ad57b889b8fedc30ef79cd9f867
chrismuehlroth <chrismuehlroth@410db64e-31f9-4487-9866-079ec192bfde>
2013-04-29 22:11:23 +0000

    Creating first drafts of UI and map

---
+++ src/webapp/js/worldmap.js
@@ -0,0 +1,15 @@
+/* Create first World Map setups */    
+/*
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+var pluginName = "worldMap",
+	defaults = {
+		apikey: '3151c38631994d539e964ce9986eac2c',
+		style: '90829',
+		center: [0,0],
+		zoom : 2,
+		maxZoom : 18,
+		minZoom: 1,
+		tileLayer : 'http://{s}.tile.cloudmade.com/{key}/{style}/256/{z}/{x}/{y}.png'
+	};
+	*/
