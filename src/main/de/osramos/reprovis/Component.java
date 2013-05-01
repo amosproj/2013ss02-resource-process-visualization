6b7a0948a6241f889983c1a80059eeb4209e7b87
shakty <shakty@410db64e-31f9-4487-9866-079ec192bfde>
2013-05-01 13:33:56 +0000

    Other idea of how to construct beans.

---
+++ src/main/de/osramos/reprovis/Component.java
@@ -0,0 +1,12 @@
+package de.osramos.reprovis;
+
+public class Component {
+	private String type;
+
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+}
---
+++ src/main/de/osramos/reprovis/GlobalServlet.java
@@ -0,0 +1,43 @@
+package de.osramos.reprovis;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+@WebServlet(name="globalServlet", urlPatterns={"/config"})
+public class GlobalServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 9060325009906441831L;
+	
+	public GlobalServlet(){
+	}
+	
+	private ArrayList<Factory> factories;
+	
+	public void init(ServletConfig config) throws ServletException{
+		super.init(config);
+		InputStream is = getServletContext().getResourceAsStream("config.json");
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			factories = mapper.readValue(is, new TypeReference<ArrayList<Factory>>(){});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+		req.getSession().setAttribute("factories", factories);
+		getServletContext().getRequestDispatcher("/config.jsp").forward(req, res);
+	}
+
+}
