3e88c55cfeca028c55da02ecf15440b7d670ea11
shakty <shakty@410db64e-31f9-4487-9866-079ec192bfde>
2013-05-02 12:01:47 +0000

    Changed servlet-url-mapping to use the web.xml because annotations are not supported by Tomcat6.

--- trunk/src/main/de/osramos/reprovis/servlets/HallServlet.java
+++ trunk/src/main/de/osramos/reprovis/servlets/HallServlet.java
@@ -3,14 +3,12 @@ package de.osramos.reprovis.servlets;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import de.osramos.reprovis.HallBean;
 
-@WebServlet(name="hallServlet", urlPatterns={"/hall"})
 public class HallServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -3606015914647184870L;
