dab161daa687fb91093470e8654f197455bfa38f
chrismuehlroth <chrismuehlroth@410db64e-31f9-4487-9866-079ec192bfde>
2013-05-09 16:56:52 +0000

    Adding licence header where they were missing

--- src/webapp/js/amos_webapp-hall.js
+++ src/webapp/js/amos_webapp-hall.js
@@ -1,3 +1,23 @@
+/*
+ - Copyright (c) 2013 by Martin Gumbrecht, Christian Muehlroth, 
+ -						Jan-Philipp Stauffert, Kathrin Koenig, Yao Guo 
+ -
+ - This file is part of the Resource Process Visualization application.
+ -
+ - This program is free software: you can redistribute it and/or modify
+ - it under the terms of the GNU Affero General Public License as
+ - published by the Free Software Foundation, either version 3 of the
+ - License, or (at your option) any later version.
+ -
+ - This program is distributed in the hope that it will be useful, 
+ - but WITHOUT ANY WARRANTY; without even the implied warranty of
+ - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ - GNU Affero General Public License for more details.
+ -
+ - You should have received a copy of the GNU Affero General Public
+ - License along with this program. If not, see
+ - <http://www.gnu.org/licenses/>.
+*/
 var svgNS = "http://www.w3.org/2000/svg";
 
 function displayHall(json){
