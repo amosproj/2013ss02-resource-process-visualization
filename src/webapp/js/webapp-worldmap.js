679a69383f27077c7aa8b65e33bb873ea33fcb43
chrismuehlroth <chrismuehlroth@410db64e-31f9-4487-9866-079ec192bfde>
2013-06-09 20:41:39 +0000

    Resolved F45. The factory information now is being reloaded after 1 minute of time interval (whereas the time interval is configurable any time).

--- src/webapp/js/webapp-worldmap.js
+++ src/webapp/js/webapp-worldmap.js
@@ -29,6 +29,8 @@ var pluginName = "worldMap",
 		startHeight: 400
 	};
 
+var worldMapMarkers = new Array();
+
 $(document).ready(function() {
 	// Set up the map
 	var map = L.map('map').setView([48.762201, 11.425374], 5);	
@@ -49,23 +51,47 @@ $(document).ready(function() {
 					 
 		return result;
 	}
-	
+		
 	// Load factories ...
-	jQuery.getJSON('./factory', function(factories) {
-		data.factories = factories;
-		// ... and draw factories into map
-		for(var i=0; i < data.factories.length; i++) {
-			var obj = data.factories[i];
-			for(var key in obj) {
+	
+	function factoryGetAll() {
+		jQuery.getJSON('./factory', function(factories) {
+			// If applicable, delete old ones
+			for(var i=0; i < worldMapMarkers.length; i++) {
+				map.removeLayer(worldMapMarkers[i]);
+			}
+			
+			// Shortcut for data
+			data.factories = factories;
+			
+			// ... and draw factories into map
+			for(var i=0; i < data.factories.length; i++) {
+				var obj = data.factories[i];
+				
+				//for(var key in obj) {
 				var markerObj = L.marker([data.factories[i].lat, data.factories[i].lon]).addTo(map)
 								.bindPopup(data.factories[i].hardTpl);
 	
+				worldMapMarkers.push(markerObj);
+				
 		        markerObj.bindPopup(factoryPopup(data.factories[i]), {offset: new L.Point(0,-10), autoPanPadding: new L.Point(10,30)});
 				if(data.factories[i].active == true || data.factories[i].active == 1)
 					markerObj.openPopup();
+				//}
 			}
-		}
-	});
+		});
+		
+		console.log("factories loaded");
+	}
+	
+	// Set interval to live-update the factories on the front map
+	// Current setting is 1 minute (1000 * 60 * 1)
+	window.onload = function(){
+		  setInterval(function() {factoryGetAll(); }, 1000 * 60 * 1);
+	};
+	
+	// Run factory setup on page load
+	factoryGetAll();
 	
 	// Bind click handler to map
 	function onMapClick(e) {
@@ -76,13 +102,6 @@ $(document).ready(function() {
 	
 	function showGlobalMap() {
 		GlobalHierarchyHandler.hierarchyZoom('global', '0');
-		/*
-		$("#canvas").animate({height: pluginConf.startHeight}, 500);
-		$("#canvas #map").animate({height: pluginConf.startHeight}, 500);
-		$("#canvas #dataLayer").animate({height: "0px", display: "none"}, 500, function() {
-			$("#canvas #dataLayer").css({display: "none"});
-		});
-		*/
 	}
 	
 	function notAvailable() {
