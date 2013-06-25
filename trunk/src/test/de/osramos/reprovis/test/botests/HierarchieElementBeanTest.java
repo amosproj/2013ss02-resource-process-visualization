66fbf11ebe0d35ef12a3a75aaa8db0329d02aab9
gumbma <gumbma@410db64e-31f9-4487-9866-079ec192bfde>
2013-06-25 17:23:47 +0000

    FEATURE 82/83: HierarchieElementBeanTest

--- trunk/src/test/de/osramos/reprovis/test/botests/HierarchieElementBeanTest.java
+++ trunk/src/test/de/osramos/reprovis/test/botests/HierarchieElementBeanTest.java
@@ -21,6 +21,117 @@
 
 package de.osramos.reprovis.test.botests;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import de.osramos.reprovis.HierarchieElementBean;
+
 public class HierarchieElementBeanTest {
+
+	@Test
+	public void getChildsTest() {
+		HierarchieElementBean hierarchieElementBean = new TestHierarchieElementBean(
+				0);
+
+		List<HierarchieElementBean> childs = hierarchieElementBean.getChilds();
+
+		assertNotNull(childs);
+		assertTrue(childs.isEmpty());
+	}
+
+	@Test
+	public void getChildsByClassTest() {
+		{
+			HierarchieElementBean hierarchieElementBean = new TestHierarchieElementBean(
+					0);
+
+			List<HierarchieElementBean> childs = hierarchieElementBean
+					.getChildsByClass(hierarchieElementBean.getClass());
+
+			assertNotNull(childs);
+			assertTrue(childs.isEmpty());
+		}
+
+		{
+			TestHierarchieElementBean hierarchieElementBean = new TestHierarchieElementBean(
+					0);
+
+			List<HierarchieElementBean> c = new ArrayList<HierarchieElementBean>();
+			c.add(new TestHierarchieElementBean(1));
+			c.add(new TestHierarchieElementBean(2));
+			hierarchieElementBean.setChilds(c);
+			{
+				List<HierarchieElementBean> childs = hierarchieElementBean
+						.getChildsByClass(HierarchieElementBean.class);
+
+				assertNotNull(childs);
+				assertTrue(childs.size() == 0);
+			}
+			{
+				List<HierarchieElementBean> childs = hierarchieElementBean
+						.getChildsByClass(TestHierarchieElementBean.class);
+
+				assertNotNull(childs);
+				assertTrue(childs.size() == 2);
+			}
+		}
+	}
+
+/*	@Test
+	public void getNumOfLeafsTest() {
+
+		{
+			HierarchieElementBean hierarchieElementBean = new TestHierarchieElementBean(
+					0);
+
+			int childs = hierarchieElementBean
+					.getNumOfLeafs(hierarchieElementBean.getClass());
+
+			assertEquals(0, childs);
+		}
+
+		{
+			TestHierarchieElementBean hierarchieElementBean = new TestHierarchieElementBean(
+					0);
+
+			List<HierarchieElementBean> c = new ArrayList<HierarchieElementBean>();
+			c.add(new TestHierarchieElementBean(1));
+			c.add(new TestHierarchieElementBean(2));
+			hierarchieElementBean.setChilds(c);
+			{
+				int childs = hierarchieElementBean
+						.getNumOfLeafs(HierarchieElementBean.class);
+
+				assertTrue(childs == 0);
+			}
+			{
+				int childs = hierarchieElementBean
+						.getNumOfLeafs(TestHierarchieElementBean.class);
+
+				assertNotNull(childs);
+				assertTrue(childs == 2);
+			}
+		}
+	}
+*/
+}
+
+class TestHierarchieElementBean extends HierarchieElementBean {
+
+	public TestHierarchieElementBean(int id) {
+		super(id);
+	}
+
+	@Override
+	protected void initChilds() {
+	}
+	
+	public void setChilds(List<HierarchieElementBean> childs) {
+		this.childs = childs;
+	}
 
 }
