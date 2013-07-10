1f0daca575c8ee446287305cd5c68bcc898b00b3
chrismuehlroth <chrismuehlroth@410db64e-31f9-4487-9866-079ec192bfde>
2013-07-10 14:12:21 +0000

    Adding KarmaJS Unit-Tests for JS objects F60-63

--- trunk/src/test/de/osramos/reprovis/test/uitests/test-playground.js
+++ trunk/src/test/de/osramos/reprovis/test/uitests/test-playground.js
@@ -34,44 +34,44 @@ describe("Global Hierarchy Handler", function() {
 	it("should zoom into global level", function() {
 		runs(function() {
 			GlobalHierarchyHandler.hierarchyZoom("global", 0);
-		})
-	})
+		});
+	});
 	
 	it("should zoom into factory level", function() {
 		runs(function() {
 			GlobalHierarchyHandler.hierarchyZoom("factory", 1);
-		})
-	})
+		});
+	});
 	
 	it("should zoom into hall level", function() {
 		runs(function() {
 			GlobalHierarchyHandler.hierarchyZoom("hall", 1);
-		})
-	})
+		});
+	});
 	
 	it("should zoom into line level", function() {
 		runs(function() {
 			GlobalHierarchyHandler.hierarchyZoom("line", 1);
-		})
-	})
+		});
+	});
 	
 	it("should zoom into location level", function() {
 		runs(function() {
 			GlobalHierarchyHandler.hierarchyZoom("location", 1);
-		})
-	})
+		});
+	});
 	
 	it("should zoom into testingDevice level", function() {
 		runs(function() {
 			GlobalHierarchyHandler.hierarchyZoom("testingDevice", 1);
-		})
-	})
+		});
+	});
 	
 	it("should zoom into component level", function() {
 		runs(function() {
 			GlobalHierarchyHandler.hierarchyZoom("component", 1);
-		})
-	})
+		});
+	});
 });
 
 describe("Navigation hierarchy", function() {
@@ -88,28 +88,97 @@ describe("Navigation hierarchy", function() {
 	});
 });
 
-describe("Hierarchie elements", function() {
+describe("Testing Device", function() {
 	it("should create the Testing Device object", function() {
 		expect(typeof TestingDevice).toBe("object");
+		
 	});
 	
+	it("should perform the AJAX data call without error", function() {
+		TestingDevice.getData(1, function(a, data) {
+			expect(data.parent).not.toBeFalsy();
+		});
+	});
+	
+	it("should have a status", function() {
+		TestingDevice.getData(1, function(a, data) {
+			expect(data.status).not.toBeFalsy();
+		});
+	});
+});
+
+describe("Factory", function() {
 	it("should create the Factory object", function() {
 		expect(typeof Factory).toBe("object");
+		
 	});
 	
+	it("should perform the AJAX data call without error", function() {
+		Factory.getData(1, function(a, data) {
+			expect(data.parent).not.toBeFalsy();
+		});
+	});
+	
+	it("should have a status", function() {
+		Factory.getData(1, function(a, data) {
+			expect(data.status).not.toBeFalsy();
+		});
+	});
+});
+
+describe("Hall", function() {
 	it("should create the Hall object", function() {
 		expect(typeof Hall).toBe("object");
+		
 	});
 	
+	it("should perform the AJAX data call without error", function() {
+		Hall.getData(1, function(a, data) {
+			expect(data.parent).not.toBeFalsy();
+		});
+	});
+	
+	it("should have a status", function() {
+		Hall.getData(1, function(a, data) {
+			expect(data.status).not.toBeFalsy();
+		});
+	});
+});
+
+describe("Line", function() {
 	it("should create the Line object", function() {
 		expect(typeof Line).toBe("object");
+		
 	});
 	
+	it("should perform the AJAX data call without error", function() {
+		Line.getData(1, function(a, data) {
+			expect(data.parent).not.toBeFalsy();
+		});
+	});
+	
+	it("should have a status", function() {
+		Line.getData(1, function(a, data) {
+			expect(data.status).not.toBeFalsy();
+		});
+	});
+});
+
+describe("Location", function() {
 	it("should create the Location object", function() {
 		expect(typeof Location).toBe("object");
+		
 	});
 	
-	it("should create the TestingDevice object", function() {
-		expect(typeof TestingDevice).toBe("object");
+	it("should perform the AJAX data call without error", function() {
+		Location.getData(1, function(a, data) {
+			expect(data.parent).not.toBeFalsy();
+		});
+	});
+	
+	it("should have a status", function() {
+		Location.getData(1, function(a, data) {
+			expect(data.status).not.toBeFalsy();
+		});
 	});
 });
